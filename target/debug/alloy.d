0fa05230794ba7f2aeb0e30fee4dc78a3c6d1c86
Hussein <qwex1210@gmail.com>
2026-04-23 00:46:23 +0300

    Cleanup: remove target directory and add to gitignore

--- target/debug/alloy.d
+++
@@ -1,1 +0,0 @@
-/home/hussein/alloy/target/debug/alloy: /home/hussein/alloy/src/Engine/mod.rs /home/hussein/alloy/src/data_modles/mod.rs /home/hussein/alloy/src/main.rs
--- target/debug/deps/alloy-a51796514ec563c6.d
+++
@@ -1,7 +0,0 @@
-/home/hussein/alloy/target/debug/deps/alloy-a51796514ec563c6.d: src/main.rs src/Engine/mod.rs src/data_modles/mod.rs
-
-/home/hussein/alloy/target/debug/deps/alloy-a51796514ec563c6: src/main.rs src/Engine/mod.rs src/data_modles/mod.rs
-
-src/main.rs:
-src/Engine/mod.rs:
-src/data_modles/mod.rs:
